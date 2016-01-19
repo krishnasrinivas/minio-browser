e97cdeb64448d9a7cceec76bcca9e276693c0886
Krishna Srinivas <krishna.srinivas@gmail.com>
2016-01-19 11:17:43 +0530

    temp

--- app/index.js
+++ app/index.js
@@ -4,14 +4,25 @@ import './js/functions.js'
 
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { Route, Router, browserHistory } from 'react-router'
+import createBrowserHistory from 'history/lib/createBrowserHistory'
+import { Route, Router, IndexRoute } from 'react-router'
 
 // import Routes from './js/components/Routes.js'
-import { Login } from './js/components/Login'
+import Login from './js/components/Login.js'
+// import Browse from './js/components/Login.js'
+
+// ReactDOM.render((
+//   <Router history={createBrowserHistory()}>
+//     <Route path='/' component={Login}>
+//       <IndexRoute component={Login} />
+//       <Route path='/browse'component={Browse} />
+//     </Route>
+//   </Router>
+// ), document.getElementById('app'))
 
 ReactDOM.render((
-  <Router history={browserHistory}>
-    <Route path="/login" handler={Login}>
+  <Router history={createBrowserHistory()}>
+    <Route path='/' component={Login}>
     </Route>
   </Router>
 ), document.getElementById('app'))
